72db4fccc316bf32347aa744ac7c859bc0a5b3d0
Plinio Balduino <pbalduino+github@gmail.com>
2012-12-06 16:04:00 -0200

    .

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,11 @@
 $(function(){
+	$("#result_content").hide();
+
+	$("#submit").click(function() {
+		var result = $.getJSON("http://api.postmon.com.br/cep/" + $("#cep").val());
+
+		console.log(result);
+
+		$("#result_content").show("slow");
+	});
 });
