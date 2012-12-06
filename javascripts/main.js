d95746ca4776503298979c08926e15dc22d02ce5
Plinio Balduino <pbalduino+github@gmail.com>
2012-12-06 16:28:17 -0200

    on sucess

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,25 @@
 $(function(){
+	function onAjaxSucess(data) {
+		$("#result_content")
+			.empty()
+			.append(JSON.stringify(data))
+			.show("slow");
+
+	}
+
+	function onAjaxError() {
+
+	}
+
+	function onSubmitClick() {
+		$("#result_content").hide("slow");
+
+		$.getJSON("http://api.postmon.com.br/cep/" + $("#cep").val()).
+				sucess(onAjaxSucess).
+				error(onAjaxError);
+	};
+
 	$("#result_content").hide();
 
-	$("#submit").click(function() {
-		var result = $.getJSON("http://api.postmon.com.br/cep/" + $("#cep").val());
-
-		console.log(result);
-
-		$("#result_content").show("slow");
-	});
+	$("#submit").click(onSubmitClick);
 });
